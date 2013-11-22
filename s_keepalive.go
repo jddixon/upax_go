02b8230a319bc36ed3e1008ddbdb0178ee3ee9e5
Jim Dixon <jddixon@gmail.com>
2013-11-22 11:57:08 -0800

    s_keepalive_test.go succeeds

--- s_keepalive.go
+++ s_keepalive.go
@@ -3,33 +3,52 @@ package upax_go
 // upax_go/s_keepalive.go
 
 import (
+	xu "github.com/jddixon/xlattice_go/util"
 	"time"
 )
 
+// Send keepalives at the specified interval.  If the lifespan is
+// greater than 0, do this that many times and then halt.  Otherwise
+// do this forever.
+//
 type KeepAliveMgr struct {
 	Interval time.Duration
+	lifespan int
+	soFar    int
 	MsgCh    chan *UpaxClusterMsg
 	StopCh   chan bool
+	DoneCh   chan bool
 }
 
-func NewKeepAliveMgr( //h *ClusterOutHandler,
-	interval time.Duration, msgCh chan *UpaxClusterMsg,
-	stopCh chan bool) (mgr *KeepAliveMgr, err error) {
+// Send keepalive messages to msgCh at the specified interval.
+// The interval should be expressed in units of time, but the lifespan
+// is the number of intervals, except that if the lifespan is less
+// than or equal to zero, the lifespan is effectively infinite.
+//
+func NewKeepAliveMgr(interval time.Duration, lifespan int,
+	msgCh chan *UpaxClusterMsg, stopCh, doneCh chan bool) (
+	mgr *KeepAliveMgr, err error) {
 
 	if msgCh == nil {
 		err = NilMsgCh
 	} else {
+		if lifespan <= 0 {
+			lifespan = xu.MAX_INT
+		}
 		mgr = &KeepAliveMgr{
 			Interval: interval,
+			lifespan: lifespan,
 			MsgCh:    msgCh,
-			StopCh:   stopCh}
+			StopCh:   stopCh,
+			DoneCh:   doneCh}
 	}
 	return
 }
 
 func (mgr *KeepAliveMgr) Run() {
 
-	for {
+	running := true
+	for running {
 		select {
 		case <-time.After(mgr.Interval):
 			op := UpaxClusterMsg_KeepAlive
@@ -39,9 +58,16 @@ func (mgr *KeepAliveMgr) Run() {
 				// actually sent.
 			}
 			mgr.MsgCh <- msgOut
+			mgr.soFar++
+			if mgr.soFar >= mgr.lifespan {
+				running = false
+			}
 
 		case <-mgr.StopCh:
-			break
+			running = false
 		}
 	}
+	// without a delay we may lose the last keepalive)
+	time.Sleep(mgr.Interval)
+	mgr.DoneCh <- true
 }
---
+++ s_keepalive_test.go
@@ -0,0 +1,51 @@
+package upax_go
+
+// upax_go/s_keepalive_test.go
+
+import (
+	"fmt"
+	xr "github.com/jddixon/xlattice_go/rnglib"
+	. "launchpad.net/gocheck"
+	"time"
+)
+
+func (s *XLSuite) TestClusterKeepAlive(c *C) {
+	if VERBOSITY > 0 {
+		fmt.Println("TEST_CLUSTER_KEEP_ALIVE")
+	}
+	rng := xr.MakeSimpleRNG()
+	k := time.Duration(1 + rng.Intn(10))
+	interval := k * time.Millisecond
+	lifespan := 3 + rng.Intn(13)
+
+	msgCh := make(chan *UpaxClusterMsg, 2*lifespan)
+	stopCh := make(chan bool)
+	doneCh := make(chan bool)
+	var msgs []*UpaxClusterMsg
+
+	mgr, err := NewKeepAliveMgr(interval, lifespan, msgCh, stopCh, doneCh)
+	c.Assert(err, IsNil)
+	go mgr.Run()
+
+	done := false
+
+	select {
+	case <-time.After(time.Duration(2*lifespan) * interval):
+		c.Fatal("timed out waiting for done from KeepAliveMgr")
+	default:
+		for !done {
+			select {
+			case msg := <-msgCh:
+				msgs = append(msgs, msg)
+			case <-doneCh:
+				done = true
+			}
+		}
+	}
+	if done {
+		c.Assert(len(msgs), Equals, lifespan)
+	} else {
+		stopCh <- true
+		<-doneCh
+	}
+}
